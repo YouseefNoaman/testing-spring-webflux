8d7408e80182d0d11228adbb02b87549eaaba00d
yosef <youseefnoaman@gmail.com>
2024-07-25 22:29:15 +0100

    Boot of the project Added mongodb connection

---
+++ src/main/java/com/example/testingreactivewebfluxspringboot/TestingReactiveWebfluxSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingreactivewebfluxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingReactiveWebfluxSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingReactiveWebfluxSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testingreactivewebfluxspringboot/TestingReactiveWebfluxSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingreactivewebfluxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingReactiveWebfluxSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
